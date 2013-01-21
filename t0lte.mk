9956408aee40fa0999da9af58a48b18c1ef4a3f5
Curtis Menard <curtis.menard@gmail.com>
2013-01-20 22:38:01 -0500

    t0lte: fix power off charging
    correct mounting of /system and move lpm.rc for t0lte.
    Signed-off-by: Curtis Menard <curtis.menard@gmail.com>
    
    Change-Id: Ic7b6d8f4cf102c0d7e047519bf541fa6b74c62b8

--- t0lte.mk
+++ t0lte.mk
@@ -30,6 +30,7 @@ PRODUCT_AAPT_PREF_CONFIG := xhdpi
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/rootdir/fstab.smdk4x12:root/fstab.smdk4x12 \
     $(LOCAL_PATH)/rootdir/init.smdk4x12.rc:root/init.smdk4x12.rc \
+    $(LOCAL_PATH)/rootdir/lpm.rc:root/lpm.rc \
     $(LOCAL_PATH)/rootdir/ueventd.smdk4x12.rc:root/ueventd.smdk4x12.rc
 
 # Audio
