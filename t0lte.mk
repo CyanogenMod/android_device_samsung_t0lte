85fc64e483d39d84745bd3abe37bb5e0ba6cdd48
Daniel Hillenbrand <codeworkx@cyanogenmod.org>
2013-05-31 14:46:07 +0200

    t0lte: default to LTE
    Change-Id: I4a8c6822673f7147ac1097dd686a7ca13368e63a

--- t0lte.mk
+++ t0lte.mk
@@ -76,7 +76,6 @@ $(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
 # RIL
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.telephony.ril_class=SamsungQualcommRIL \
-    telephony.lteOnGsmDevice=1 \
     mobiledata.interfaces=pdp0,wlan0,gprs,ppp0 \
     ro.ril.hsxpa=1 \
     ro.ril.gprsclass=10
