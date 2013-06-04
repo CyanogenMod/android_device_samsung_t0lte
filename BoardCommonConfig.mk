769c90dd55360c3f66b5fbc38c3dde27a10d1623
sbrissen <sbrissen@hotmail.com>
2013-06-04 14:19:37 -0400

    t0lte: merge in t0ltecdma
    with minor changes to i605, l900, and r950, this will make the
    t0ltecdma repo obsolete.
    
    Change-Id: I5b28c956327856f2aa2bbf244a2b3156743d6097

--- BoardCommonConfig.mk
+++ BoardCommonConfig.mk
@@ -31,7 +31,11 @@ COMMON_GLOBAL_CFLAGS += -DCAMERA_WITH_CITYID_PARAM
 
 # Kernel
 TARGET_KERNEL_SOURCE := kernel/samsung/smdk4412
+ifneq ($(filter t0ltecdma i605 l900 r950,$(TARGET_DEVICE)),)
 TARGET_KERNEL_CONFIG := cyanogenmod_t0lte_defconfig
+else
+TARGET_KERNEL_CONFIG := cyanogenmod_t0ltecdma_defconfig
+endif
 
 # assert
 TARGET_OTA_ASSERT_DEVICE := t0lte,t0ltexx,GT-N7105,t0ltedv,GT-N7105T,t0lteatt,SGH-I317,t0ltetmo,SGH-T889,t0ltecan,t0ltevl,SGH-I317M
---
+++ common.mk
@@ -0,0 +1,85 @@
+#
+# Copyright (C) 2013 The CyanogenMod Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+LOCAL_PATH := device/samsung/t0lte
+
+# Overlay
+ifneq ($(filter t0ltecdma i605 l900 r950,$(TARGET_DEVICE)),)
+DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
+else
+DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay-cdma
+endif
+
+# This device is xhdpi.  However the platform doesn't
+# currently contain all of the bitmaps at xhdpi density so
+# we do this little trick to fall back to the hdpi version
+# if the xhdpi doesn't exist.
+PRODUCT_AAPT_CONFIG := normal hdpi xhdpi
+PRODUCT_AAPT_PREF_CONFIG := xhdpi
+
+# Audio
+PRODUCT_PACKAGES += \
+    tiny_hw
+
+# Product specific Packages
+PRODUCT_PACKAGES += \
+    GalaxyNote2Settings
+
+# NFC
+PRODUCT_PACKAGES += \
+    nfc.exynos4 \
+    libnfc \
+    libnfc_jni \
+    Nfc \
+    Tag
+
+PRODUCT_COPY_FILES += \
+    packages/apps/Nfc/migrate_nfc.txt:system/etc/updatecmds/migrate_nfc.txt \
+    frameworks/base/nfc-extras/com.android.nfc_extras.xml:system/etc/permissions/com.android.nfc_extras.xml \
+    frameworks/native/data/etc/android.hardware.nfc.xml:system/etc/permissions/android.hardware.nfc.xml
+
+# NFCEE access control
+ifeq ($(TARGET_BUILD_VARIANT),user)
+    NFCEE_ACCESS_PATH := $(LOCAL_PATH)/configs/nfcee_access.xml
+else
+    NFCEE_ACCESS_PATH := $(LOCAL_PATH)/configs/nfcee_access_debug.xml
+endif
+
+PRODUCT_COPY_FILES += \
+    $(NFCEE_ACCESS_PATH):system/etc/nfcee_access.xml
+
+PRODUCT_PACKAGES += \
+    com.android.nfc_extras \
+    Stk \
+    SamsungServiceMode
+
+$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
+
+# RIL
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.telephony.ril_class=SamsungQualcommRIL \
+    mobiledata.interfaces=pdp0,wlan0,gprs,ppp0
+
+# These are the hardware-specific features
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/handheld_core_hardware.xml:system/etc/permissions/handheld_core_hardware.xml \
+    frameworks/native/data/etc/android.hardware.sensor.barometer.xml:system/etc/permissions/android.hardware.sensor.barometer.xml
+
+# Include common makefile
+$(call inherit-product, device/samsung/smdk4412-common/common.mk)
+$(call inherit-product, device/samsung/smdk4412-qcom-common/common.mk)
+
+$(call inherit-product-if-exists, vendor/samsung/t0lte/t0lte-vendor.mk)
