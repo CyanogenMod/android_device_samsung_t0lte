5971fcc381743943a6621dd152d21dbe92e0879b
sbrissen <sbrissen@hotmail.com>
2013-06-10 11:04:08 -0400

    t0lte: user configureable voice/streaming volumes [1/2]
    This uses 4 files to store the voice volume:
    incall_earpiece, incall_headphone, incall_speaker, incall_bt, and 2
    for streaming volume: out_speaker, out_headphone located /data/local/audio.
    
    Change-Id: I4269dc65d48e04b5e908b20704128241683d5a80

--- audio/audio_hw.h
+++ audio/audio_hw.h
@@ -66,13 +66,25 @@
 /* sampling rate when using VX port for wide band */
 #define VX_WB_SAMPLING_RATE 16000
 
-/* product-specific defines */
-#define PRODUCT_DEVICE_PROPERTY "ro.product.device"
-#define PRODUCT_NAME_PROPERTY   "ro.product.name"
+#define MAX_NUM_VOLUME_FILES 6
+#define AUDIO_DIR "/data/local/audio"
+
+/* in-call files */
+#define INCALL_EARPIECE "/data/local/audio/incall_earpiece"
+#define INCALL_HEADPHONE "/data/local/audio/incall_headphone"
+#define INCALL_SPEAKER "/data/local/audio/incall_speaker"
+#define INCALL_BT "/data/local/audio/incall_bt"
+
+/* out stream files */
+#define OUT_HEADPHONE "/data/local/audio/out_headphone"
+#define OUT_SPEAKER "/data/local/audio/out_speaker"
 
 #define ARRAY_SIZE(a) (sizeof(a) / sizeof((a)[0]))
 
 #define STRING_TO_ENUM(string) { #string, string }
+
+#define DB_TO_HEADPHONE_VOLUME(x) (((x) + 30) / 2)
+#define DB_TO_SPEAKER_VOLUME(x) (((x) + 52) / 2)
 
 struct string_to_enum {
     const char *name;
@@ -104,6 +116,15 @@ enum tty_modes {
     TTY_MODE_VCO,
     TTY_MODE_HCO,
     TTY_MODE_FULL
+};
+
+char *volume_file[MAX_NUM_VOLUME_FILES] = {
+    INCALL_EARPIECE,
+    INCALL_HEADPHONE,
+    INCALL_SPEAKER,
+    INCALL_BT,
+    OUT_HEADPHONE,
+    OUT_SPEAKER
 };
 
 /* ACDB Device ID macros */
@@ -157,6 +178,8 @@ struct mixer_ctls
 {
     struct mixer_ctl *mixinl_in1l_volume;
     struct mixer_ctl *mixinl_in2l_volume;
+    struct mixer_ctl *speaker_volume;
+    struct mixer_ctl *headphone_volume;
 };
 
 struct route_setting
