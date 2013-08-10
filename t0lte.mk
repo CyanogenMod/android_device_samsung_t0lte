02b0584a02607bbd9a35348b30990ecdf8b93aae
Daniel Hillenbrand <codeworkx@cyanogenmod.org>
2013-08-10 17:47:23 +0200

    t0lte: use unified init.smdk4x12.rc
    Change-Id: Iad26059d1468f5b3f5da569b1767f0e2e2d0715e

--- t0lte.mk
+++ t0lte.mk
@@ -19,7 +19,7 @@ LOCAL_PATH := device/samsung/t0lte
 # Init files
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/rootdir/fstab.smdk4x12:root/fstab.smdk4x12 \
-    $(LOCAL_PATH)/rootdir/init.smdk4x12.rc:root/init.smdk4x12.rc \
+    $(LOCAL_PATH)/rootdir/init.target.rc:root/init.target.rc \
     $(LOCAL_PATH)/rootdir/lpm.rc:root/lpm.rc \
     $(LOCAL_PATH)/rootdir/ueventd.smdk4x12.rc:root/ueventd.smdk4x12.rc
 
