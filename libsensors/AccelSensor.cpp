3a2eaf1db9cbfb77149f832925aec3e017a425cf
sbrissen <sbrissen@hotmail.com>
2013-11-23 16:07:08 -0500

    t0lte: opensource sensors
    These are a work in progress....
    -all sensors turn on and report data
    -Sensor data needs to be checked for accuracy
    -Source needs to be cleaned up before merging
    
    Change-Id: I5c79b039ce4812ba82f5f257624c7bb9ac455801

---
+++ libsensors/AccelSensor.cpp
@@ -0,0 +1,167 @@
+/*
+ * Copyright (C) 2008 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <fcntl.h>
+#include <errno.h>
+#include <math.h>
+#include <poll.h>
+#include <unistd.h>
+#include <dirent.h>
+#include <sys/select.h>
+#include <cutils/log.h>
+
+#include "AccelSensor.h"
+
+/*****************************************************************************/
+AccelSensor::AccelSensor()
+    : SensorBase(NULL, "accelerometer_sensor"),
+      mEnabled(0),
+
+      mInputReader(4),
+      mHasPendingEvent(false)
+{
+   // ALOGD("AccelSensor::AccelSensor()");
+    mPendingEvent.version = sizeof(sensors_event_t);
+    mPendingEvent.sensor = ID_A;
+    mPendingEvent.type = SENSOR_TYPE_ACCELEROMETER;
+    memset(mPendingEvent.data, 0, sizeof(mPendingEvent.data));
+    
+ //   ALOGD("AccelSensor::AccelSensor() open data_fd");
+	
+    if (data_fd) {
+        strcpy(input_sysfs_path, "/sys/class/input/");
+        strcat(input_sysfs_path, input_name);
+        strcat(input_sysfs_path, "/device/");
+        input_sysfs_path_len = strlen(input_sysfs_path);
+
+        //enable(0, 1);
+    }
+    //    ALOGE("AccelSensor: sysfs: %s",input_sysfs_path);
+}
+
+AccelSensor::~AccelSensor() {
+
+  //  ALOGD("AccelSensor::~AccelSensor()");
+    if (mEnabled) {
+        enable(0, 0);
+    }
+}
+
+int AccelSensor::enable(int32_t, int en) {	   
+  //  ALOGD("AccelSensor::~enable(0, %d)", en);
+    int flags = en ? 1 : 0;
+    if (flags != mEnabled) {
+        int fd;
+
+        fd = open(SSP_DEVICE_ENABLE, O_RDWR);
+        if (fd >= 0) {
+            char buf[2];
+            int err;
+            buf[1] = 0;
+            if (flags) {
+                err = write(fd, "111", 10);
+			} else {
+			    err = write(fd, "1", 10);
+            }          
+            close(fd);
+            mEnabled = flags;
+
+            return 0;
+        }
+        return -1;        
+    }
+    return 0;
+}
+
+
+bool AccelSensor::hasPendingEvents() const {
+    /* FIXME probably here should be returning mEnabled but instead
+	mHasPendingEvents. It does not work, so we cheat.*/
+    //ALOGD("AccelSensor::~hasPendingEvents %d", mHasPendingEvent ? 1 : 0 );
+    return mHasPendingEvent;
+}
+
+
+int AccelSensor::setDelay(int32_t handle, int64_t ns)
+{
+  //  ALOGD("AccelSensor::~setDelay(%d, %lld)", handle, ns);
+
+    int fd;
+
+    if (ns < 10000000) {
+        ns = 10000000; // Minimum on stock
+    }
+
+    strcpy(&input_sysfs_path[input_sysfs_path_len], "accel_poll_delay");
+    fd = open(input_sysfs_path, O_RDWR);
+    if (fd >= 0) {
+        char buf[80];
+        sprintf(buf, "%lld", ns / 10000000 * 10); // Some flooring to match stock value
+        write(fd, buf, strlen(buf)+1);
+        close(fd);
+        return 0;
+    }
+    return -1;
+}
+
+
+int AccelSensor::readEvents(sensors_event_t* data, int count)
+{
+    //ALOGD("AccelSensor::~readEvents() %d", count);
+    if (count < 1)
+        return -EINVAL;
+   
+    if (mHasPendingEvent) {
+        mHasPendingEvent = false;
+        mPendingEvent.timestamp = getTimestamp();
+        *data = mPendingEvent;
+        return mEnabled ? 1 : 0;
+    }
+   
+    ssize_t n = mInputReader.fill(data_fd);
+    if (n < 0)
+        return n;
+    int numEventReceived = 0;
+    input_event const* event;
+
+    while (count && mInputReader.readEvent(&event)) {
+        int type = event->type;
+        if (type == EV_REL) {
+            float value = event->value;
+            if (event->code == EVENT_TYPE_ACCEL_X) {
+                mPendingEvent.acceleration.x = value * CONVERT_A_X;
+            } else if (event->code == EVENT_TYPE_ACCEL_Y) {
+                mPendingEvent.acceleration.y = value * CONVERT_A_Y;
+            } else if (event->code == EVENT_TYPE_ACCEL_Z) {
+                mPendingEvent.acceleration.z = value * CONVERT_A_Z;
+            }
+        } else if (type == EV_SYN) {
+            mPendingEvent.timestamp = timevalToNano(event->time);
+            if (mEnabled) {
+                *data++ = mPendingEvent;
+                count--;
+                numEventReceived++;
+            }
+        } else {
+            ALOGE("AccelSensor: unknown event (type=%d, code=%d)",
+                    type, event->code);
+        }
+
+        mInputReader.next();
+    }
+	return numEventReceived++;
+		
+}
