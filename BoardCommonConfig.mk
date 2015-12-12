5a1f21fb748bf64035a2211621914dabdcbfd6f2
rogersb11 <brettrogers11@gmail.com>
2015-12-11 21:01:29 -0500

    t0lte: Enable F2FS support
    Change-Id: Ie99a80a538ba3bfb15b76023e90fc0421178ecfb

--- BoardCommonConfig.mk
+++ BoardCommonConfig.mk
@@ -40,6 +40,14 @@ endif
 TARGET_RECOVERY_FSTAB := device/samsung/t0lte/rootdir/fstab.smdk4x12
 RECOVERY_FSTAB_VERSION := 2
 
+# F2FS
+PRODUCT_PACKAGES += \
+    fibmap.f2fs \
+    fsck.f2fs \
+    mkfs.f2fs
+
+TARGET_USERIMAGES_USE_F2FS := true
+
 # assert
 TARGET_OTA_ASSERT_DEVICE := t0lte,t0ltexx,GT-N7105,t0ltedv,GT-N7105T,t0lteatt,SGH-I317,t0ltetmo,SGH-T889,t0ltecan,t0ltevl,SGH-I317M
 
